82e0c4af67920eff7efa2965d0fb85fc0d167cb5
Hassan <hassanmrefaat@gmail.com>
2025-11-26 21:28:14 +0200

    FactoryController

---
+++ PresentationLayer/Controllers/FactoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentaionLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FactoryController : ControllerBase
+    {
+    }
+}
