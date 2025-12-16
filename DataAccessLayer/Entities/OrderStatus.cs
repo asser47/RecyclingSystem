c02550c605ec6800a2cdff8e683b2c32f2140223
KhaledSalem4 <khaledas418@gmail.com>
2025-12-16 05:23:29 +0200

    Fix OrderStatus enum: Replace Accepted/Collected with Assigned/InProgress to match service logic

--- DataAccessLayer/Entities/OrderStatus.cs
+++ DataAccessLayer/Entities/OrderStatus.cs
@@ -9,8 +9,8 @@ namespace DataAccessLayer.Entities
     public enum OrderStatus
     {
         Pending = 0,        // Order created, waiting for collector
-        Accepted = 1,       // Collector accepted the order
-        Collected = 2,     // Collector picked up materials
+        Assigned = 1,       // Collector accepted/assigned to the order
+        InProgress = 2,     // Collector is picking up materials
         Delivered = 3,      // Materials delivered to factory
         Completed = 4,      // Order completed, points awarded
         Cancelled = 5       // Order cancelled
